41a01e724e3687e6f74fd96c12f0f450566ef4f2
Ojaswi bhardwaj <ojaswibhardwaj86@gmail.com>
2026-05-26 22:48:52 +0530

    Fixed

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/Rock_Paper_Scissor/",
+});
